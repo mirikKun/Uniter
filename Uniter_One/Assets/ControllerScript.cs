0d760827a9b1916d88b0c4a309d27267b0684874
Андрій Мірошник <55330163+mirikKun@users.noreply.github.com>
2021-03-26 16:46:07 +0200

    Movement_Gun

---
+++ Uniter_One/Assets/ControllerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform groundCheck;
+    public float gravity=-20f;
+    public LayerMask groundMasc;
+    public float groundDistance = 0.5f;
+    public float speed = 12f;
+    public float jumpHeight = 10;
+
+    Vector3 gravityVelocity;
+    bool isGrounded;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Gravity();
+        Jump();
+    }
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+    void Gravity()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance,groundMasc);
+        if(isGrounded && gravityVelocity.y<0)
+        {
+            gravityVelocity.y = -3;
+        }
+        controller.Move(gravityVelocity * Time.deltaTime);
+       
+    }
+    void Jump()
+    {
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            gravityVelocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+        gravityVelocity.y += gravity * Time.deltaTime;
+    }
+}
---
+++ Uniter_One/Assets/GunScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public float damage = 10;
+    public float range = 100;
+    public Camera fpCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+            {
+            Shoot();
+        }
+    }
+    void  Shoot()
+    {
+        RaycastHit hit;
+       if( Physics.Raycast(fpCam.transform.position, fpCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
