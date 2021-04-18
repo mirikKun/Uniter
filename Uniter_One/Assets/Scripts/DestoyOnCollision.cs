989b9a884988d803b955b45addaf4550045875f3
Андрій Мірошник <55330163+mirikKun@users.noreply.github.com>
2021-04-18 18:00:36 +0300

    ScillsCooldown

---
+++ Uniter_One/Assets/Scripts/DestoyOnCollision.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoyOnCollision : MonoBehaviour
+{
+    public GameObject mussleFlash;
+    public GameObject hitFlash;
+    
+    private void Start()
+    {
+        GameObject flash = Instantiate(mussleFlash, transform.position, transform.localRotation);
+        Destroy(flash,1);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+
+        ContactPoint contact = other.contacts[0];
+        Quaternion rot=Quaternion.FromToRotation(Vector3.up,contact.normal);
+        Vector3 pos = contact.point;
+
+        GameObject hit = Instantiate(hitFlash, pos, rot);
+        
+        Destroy( hit,3);
+if (!other.gameObject.CompareTag("Enemy")&&!other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Uniter_One/Assets/Scripts/SkillController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillController : MonoBehaviour
+{
+    public Image[] imageCooldowns;
+
+    public void OuterStartCoroutine(int index, float cooldown)
+    {
+        StartCoroutine(StartCooldown(index, cooldown));
+    }
+    public IEnumerator StartCooldown(int index, float cooldown)
+    {
+        imageCooldowns[index].fillAmount = 0 ;
+        while (imageCooldowns[index].fillAmount != 1)
+        {
+            imageCooldowns[index].fillAmount += 1 / cooldown * Time.deltaTime;
+            yield return null;
+        }
+        yield return null;
+    }
+}
