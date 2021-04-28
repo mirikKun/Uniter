62de1b19c1383a4f57d1497e58b65563bd6eb5e3
Андрій Мірошник <55330163+mirikKun@users.noreply.github.com>
2021-04-28 18:11:30 +0300

    TryMultilpayer2

---
+++ Uniter_One/Assets/PlayerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+
+public class PlayerManager : MonoBehaviour
+{
+    private PhotonView PV;
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    void Start()
+    {
+        if (PV.IsMine)
+        {
+            CreateController(); 
+        }
+    }
+
+    void CreateController()
+    {
+        Debug.Log("InstantiatePlayer");
+        //Instantiating
+    }
+}
